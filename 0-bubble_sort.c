a5acb200dd9be0ade720c90480fcbdbc1cd16ce1
aya-cs22 <aya786930@gmail.com>
2024-01-22 15:52:57 +0200

    Sorting algorithms & Big O

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,10 +8,8 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-	if (!size || !array)
-	{
+	if (size < 2)
 		return;
-	}
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
