59bc51f5befa77248bc8164eafadfe26e4852dc1
aya-cs22 <aya786930@gmail.com>
2024-01-23 20:55:06 +0200

    Sorting algorithms & Big O

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+/**
+ * selection_sort - a function that sorts an array of integers
+ * @array: array integer
+ * @size: size of array
+*/
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int min;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				_swap(&array[j], &array[min]);
+				print_array(array, size);
+			}
+		}
+	}
+}
+/**
+ * _swap - the values of two integers.
+ * @a: a Pointer to the first integer
+ * @b: a Pointer to the second integer
+*/
+void _swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
